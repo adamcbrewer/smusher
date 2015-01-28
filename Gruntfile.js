c2019ce729261f4196545eb4c0d3cb62687b68ea
adam.brewer <adam.brewer@burberry.com>
2015-01-28 14:45:52 +0000

    Init files

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    require('time-grunt')(grunt);
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        project: {
+            input: 'input',
+            output: 'output'
+        },
+
+        clean: {
+            input: [
+                '<%= project.input %>/*'
+            ],
+            output: [
+                '<%= project.output %>/*'
+            ]
+        },
+
+        imagemin: {
+            input: {
+                options: {
+                    optimizationLevel: 4,
+                    progressive: true,
+                    pngquant: true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= project.input %>',
+                        src: [
+                            '*.{png,jpg,jepg,gif}',
+                            '**/*.{png,jepg,jpg,gif}',
+                        ],
+                        dest: '<%= project.output %>'
+                    }
+                ]
+            }
+        },
+
+        watch: {
+            options: {
+                event: ['changed', 'added', 'deleted']
+            },
+            images: {
+                options: {
+                    event: ['changed', 'added', 'deleted']
+                },
+                files: '<%= project.input %>/{,*/}*',
+                tasks: [
+                    'imagemin'
+                ]
+            }
+        }
+    });
+
+    grunt.registerTask('default', [ 'imagemin' ]);
+
+    grunt.registerTask('reset', [ 'clean' ]);
+
+};
